e039b41bd1eb7a0093a25d8fb7ec04c59aadde35
Amélia SZK <amelia.szk@protonmail.com>
2021-09-03 20:03:54 -0400

    We can read the input CSV

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,15 @@
 # A file to experiment with stuff in the Python language
 
+import csv
 
+input_filename = "Squadron_semicolons.csv"
+
+#https://docs.python.org/3/library/csv.html#csv.DictReader
+with open(input_filename, newline='') as csvfile:
+    dialect = csv.Sniffer().sniff(csvfile.read())
+    csvfile.seek(0)
+    datareader = csv.DictReader(csvfile, dialect=dialect)
+    for row in datareader:
+        print(row)
 
 
